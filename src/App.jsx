2d01d629c41ef64139e1da05ba05726f87d0e8e1
Justin <justinellis@Justin-MacBookPro.local>
2023-11-30 16:27:19 -0700

    Finished course with different aspects of React

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
-import SearchParams from './SearchParams';
+import React, { useState } from "react";
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AdoptedPetContext from "./AdoptedPetContext";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
+
+const queryCLient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
+  const adoptedPet = useState(null);
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
-  )
+    <BrowserRouter>
+      <QueryClientProvider client={queryCLient}>
+        <AdoptedPetContext.Provider value={adoptedPet}>
+          <header>
+            <Link to="/"> Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </AdoptedPetContext.Provider>
+      </QueryClientProvider>
+    </BrowserRouter>
+  );
 };
 
-export default App
+export default App;
